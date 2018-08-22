073a4896069e1ca15685e221cd1270318e7647a0
Forest Hoffman <fhoffman@coder.com>
2018-08-22 14:15:12 -0500

    Rename some functions

--- index.js
+++ index.js
@@ -1,57 +1,43 @@
 (function() {
-    const cookieSpawnChance = 0.5; // 50% chance to spawn a cookie
     const cookieWidth = 64;
     const startY = -cookieWidth;
     const endY = 730;
     const cookieCount = 2;
+    const cookieSpawnChance = 0.5;
     let score, start;
-    let scoreEl = document.querySelector("#scoreVal")
+    let scoreEl = document.querySelector("#scoreVal");
     let timeEl = document.querySelector("#timeVal");
-    let content = document.querySelector("#playground");
+    let playground = document.querySelector("#playground");
 
-    // Starts the game.
     function startGame() {
-        // Resets the game.
-        content.classList.remove("game-over");
-        content.onclick = null;
+        playground.classList.remove("game-over");
+        playground.onclick = null;
         score = 0;
         start = null;
         drawTime();
-        toggleScoreColor();
         drawScore();
+        toggleScoreColor();
 
-        // Show the info text for a few seconds.
-        content.classList.add("pre-start");
+        playground.classList.add("pre-start");
         setTimeout(function() {
-            content.classList.remove("pre-start");
+            playground.classList.remove("pre-start");
         }, 5000);
 
-        // Initialize the cookies and their animations.
+        // Create our cookies.
         for (let x = 0; x < cookieCount; x++) {
             spawnCookie();
         }
         window.requestAnimationFrame(draw);
     }
 
-    // Generate a new cookie and add it to the DOM.
-    function genCookie() {
-        let cookie = document.createElement('div');
-        cookie.classList.add('cookie');
-        content.appendChild(cookie);
-        return cookie;
+    function getRandLeft() {
+        return Math.random() * (720 - cookieWidth);
     }
 
-    // Assign animations and event listeners to a new cookie.
-    function spawnCookie() {
-        let cookie = genCookie();
-
-        // Randomly animate the cookie in the next couple milliseconds.
-        animateAfterTimeout(cookie, getRandTimeout());
-
-        cookie.onmousedown = eatCookie;
+    function getRandTimeout() {
+        return Math.random() * 2000;
     }
 
-    // Update the displayed time, above the playground.
     function drawTime(t) {
         let minutes = Math.floor(t / 60000);
         if (minutes < 10) {
@@ -64,117 +50,101 @@
         timeEl.innerHTML = `${minutes}:${seconds}`;
     }
 
-    // Updates the score color, based on whether the score
-    // has been increased or decreased.
-    function toggleScoreColor(type) {
-        switch (type) {
-            case "inc":
-                scoreEl.classList.add('positive');
-                scoreEl.classList.remove('negative');
-                break;
-            case "dec":
-                scoreEl.classList.add('negative');
-                scoreEl.classList.remove('positive');
-                break;
-            default:
-                scoreEl.classList.remove('negative');
-                scoreEl.classList.remove('positive');
-        }
-    }
-
-    // Update the displayed score, above the playground.
     function drawScore() {
         scoreEl.innerHTML = score;
     }
 
-    // Increment the score, and update the display.
+    function toggleScoreColor(type) {
+        switch (type) {
+            case "inc":
+                scoreEl.classList.add("positive");
+                scoreEl.classList.remove("negative");
+                break;
+            case "dec":
+                scoreEl.classList.add("negative");
+                scoreEl.classList.remove("positive");
+                break;
+            default:
+                scoreEl.classList.remove("negative");
+                scoreEl.classList.remove("positive");
+        }
+    }
+
     function increaseScore() {
         score++;
         drawScore();
         toggleScoreColor("inc");
     }
 
-    // Decrement the score (if positive), and updat the display.
     function decreaseScore() {
         if (score > 0) score--;
         drawScore();
         toggleScoreColor("dec");
     }
 
-    // Get a psuedo-random value for displacing a cookie horizontally.
-    function getRandLeft() {
-        return Math.random() * (720 - cookieWidth);
-    }
-
-    // Get a psuedo-random value for delaying a cookie's animation.
-    function getRandTimeout() {
-        return Math.random() * 2000;
-    }
-
-    // Animate the target cookie, after a delay, independently of
-    // other cookies.
     async function animateAfterTimeout(el, ms) {
         let rand = getRandLeft();
         el.style.transform = `translate(${rand}px)`;
         setTimeout(function() {
-            el.classList.add('animate');
+            el.classList.add("animate");
         }, ms);
     }
 
-    // Might generate a cookie. Might not.
-    function quantumCookie() {
+    function spawnCookie() {
+        let cookie = document.createElement("div");
+        cookie.classList.add("cookie");
+        playground.appendChild(cookie);
+
+        animateAfterTimeout(cookie, getRandTimeout());
+        cookie.onmousedown = eatCookie;
+        cookie.onclick = eatCookie;
+    }
+
+    function randomCookie() {
         if (Math.random() > cookieSpawnChance) {
             spawnCookie();
         }
     }
 
-    // Removes a cookie and increases the score.
     function eatCookie(e) {
         let el = e.target;
-        if (el.classList.contains('animate')) {
-            el.classList.remove('animate');
+        if (el.classList.contains("animate")) {
+            el.classList.remove("animate");
         } else {
             animateAfterTimeout(el, 100);
         }
         increaseScore();
-        quantumCookie();
+
+        randomCookie();
     }
 
-    // Ends the game.
     function gameOver() {
-        content.classList.add("game-over");
+        playground.classList.add("game-over");
 
-        let cookies = document.querySelectorAll('.cookie');
+        let cookies = document.querySelectorAll(".cookie");
         if (cookies.length > 1) {
             for (let i = 0; i < cookies.length; i++) {
                 cookies[i].remove();
             }
         }
 
-        // Assign the click listener after 2 seconds,
-        // to give the user time to stop clicking.
         setTimeout(function() {
-            content.onclick = function() {
-                startGame();
-            };
+            playground.onclick = startGame;
         }, 2000);
     }
 
-    // Runs per frame of the game.
     function draw(timestamp) {
         if (!start) start = timestamp;
         let progress = timestamp - start;
-        let cookies = document.querySelectorAll('.cookie');
+        let cookies = document.querySelectorAll(".cookie");
         for (let i = 0; i < cookies.length; i++) {
-            if (!cookies[i].classList.contains('animate') && cookies[i].offsetTop === startY) {
+            if (!cookies[i].classList.contains("animate") && cookies[i].offsetTop === startY) {
                 animateAfterTimeout(cookies[i], getRandTimeout());
             } else if (cookies[i].offsetTop === endY) {
-                cookies[i].classList.remove('animate');
+                cookies[i].classList.remove("animate");
                 decreaseScore();
             }
 
-            // It's Game Over if the grace period has passed and
-            // the score is still zero.
             if (progress > 5000 && score === 0) {
                 gameOver();
                 return;
